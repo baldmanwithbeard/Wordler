cff8985b34b940d49c3c8bcecdc6893cec638045
Sergey Koryakin <sergeykoryakinofficial@gmail.com>
2023-05-28 06:37:40 -0500

    Add project files.

---
+++ Wordler.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Wordle Solver!");
+
+// Your code for Wordle solving logic goes here
+
+Console.WriteLine("Thank you for using Wordle Solver. Press any key to exit.");
+Console.ReadKey();
