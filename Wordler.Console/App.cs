b194f30b48c19bf03283093152e3030ceb312483
Sergey Koryakin <sergeykoryakinofficial@gmail.com>
2023-05-28 08:55:12 -0500

    parameterless Run for developing the simple app code

--- Wordler.Console/App.cs
+++ Wordler.Console/App.cs
@@ -11,6 +11,14 @@ internal class App
         _messages = messages;
     }
 
+    public void Run()
+    {
+        Console.WriteLine("Welcome to Wordle Solver!");
+        // Your code for Wordle solving logic goes here
+        Console.WriteLine("Thank you for using Wordle Solver. Press any key to exit.");
+        Console.ReadKey();
+    }
+
     public void Run(IEnumerable<string> args)
     {
         var languageCode = args.FirstOrDefault(a => a.ToLower().StartsWith("lang="))?[5..] ?? "en";
--- Wordler.Console/Program.cs
+++ Wordler.Console/Program.cs
@@ -5,22 +5,23 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Wordler.ConsoleApp;
 
-//Console.WriteLine("Welcome to Wordle Solver!");
-
-//// Your code for Wordle solving logic goes here
-
-//Console.WriteLine("Thank you for using Wordle Solver. Press any key to exit.");
-//Console.ReadKey();
-
 using var host = CreateHostBuilder(args).Build();
 using var scope = host.Services.CreateScope();
 
+var usingArgs = false;
 var services = scope.ServiceProvider;
 
 try
 {
-    services.GetRequiredService<App>().Run(args);
+    if (usingArgs)
+    {
+        services.GetRequiredService<App>().Run(args);
+        return;
+    }
+
+    services.GetRequiredService<App>().Run();
 }
+// rethrows any exception from the app as a console message
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
