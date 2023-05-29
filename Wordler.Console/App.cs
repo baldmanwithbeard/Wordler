8ef1a20ccd5fd85f49320848d2739f2b864f05b4
Sergey Koryakin <sergeykoryakinofficial@gmail.com>
2023-05-29 01:27:20 -0500

    adding "Game" class

--- Wordler.Console/App.cs
+++ Wordler.Console/App.cs
@@ -13,12 +13,23 @@ internal class App
 
     public void Run()
     {
-        Console.WriteLine("Welcome to Wordle Solver!");
+        Console.WriteLine("Welcome to Wordle Solver!" + Environment.NewLine +
+                          "If this is your first time, please type \"help\", otherwise hit Enter to begin");
+        if (Console.ReadLine()?.ToLower() == "help") ProvideHelp();
+
         // Your code for Wordle solving logic goes here
         Console.WriteLine("Thank you for using Wordle Solver. Press any key to exit.");
         Console.ReadKey();
     }
 
+    private static void ProvideHelp()
+    {
+        Console.WriteLine("ask chatgpt you buffoon. loljk, i just ain't written it out yet, sry");
+
+        Console.ReadLine();
+    }
+
+    //for the last two, i might want an "Answer" class that keeps track of the solved positions in the word, as well as the letters than can be
     public void Run(IEnumerable<string> args)
     {
         var languageCode = args.FirstOrDefault(a => a.ToLower().StartsWith("lang="))?[5..] ?? "en";
---
+++ Wordler.Console/Game.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Wordler.ConsoleApp
+{
+    internal class Game
+    {
+        private const int MaxAttempts = 6;
+        private int attempts = 0;
+        private List<char> unguessedLetters;
+        private List<char> incorrectLetters;
+        private Dictionary<char, List<int>> misplacedLetters;
+        private char[] answer;
+
+        private Dictionary<string, double> heterogramScores;
+
+        public Game()
+        {
+            unguessedLetters = new List<char>("abcdefghijklmnopqrstuvwxyz");
+            incorrectLetters = new List<char>();
+            misplacedLetters = new Dictionary<char, List<int>>();
+            answer = new char[5];
+            for (int i = 0; i < 5; i++)
+            {
+                answer[i] = '_';
+            }
+
+            // Load heterogram_scores.json
+            heterogramScores = JsonConvert.DeserializeObject<Dictionary<string, double>>(
+                File.ReadAllText("heterogram_scores.json"));
+        }
+
+        public void Guess(string guess)
+        {
+            if (attempts >= MaxAttempts)
+            {
+                Console.WriteLine("Maximum attempts reached!");
+                return;
+            }
+
+            var feedback = GetFeedback();
+
+            // Update the game state based on the guess
+            for (var i = 0; i < 5; i++)
+            {
+                switch (feedback[i])
+                {
+                    case '!':
+                        answer[i] = guess[i];
+                        break;
+
+                    case '-' when !misplacedLetters.ContainsKey(guess[i]):
+                        misplacedLetters[guess[i]] = new List<int> { i };
+                        break;
+
+                    case '-':
+                        misplacedLetters[guess[i]].Add(i);
+                        break;
+
+                    default:
+                        incorrectLetters.Add(guess[i]);
+                        break;
+                }
+
+                unguessedLetters.Remove(guess[i]);
+            }
+
+            attempts++;
+
+            if (IsWin())
+            {
+                Console.WriteLine("Congratulations, you've won!");
+            }
+            else if (attempts >= MaxAttempts)
+            {
+                Console.WriteLine("Game over, better luck next time!");
+            }
+        }
+
+        private static string GetFeedback()
+        {
+            var feedback = "";
+            do
+            {
+                Console.WriteLine("Enter your feedback for the guess");
+                feedback = Console.ReadLine() ?? "";
+            }
+            while (!ValidateInput(feedback));
+
+            return feedback;
+        }
+
+        private static bool ValidateInput(string input)
+        {
+            var isValidInput = Regex.IsMatch(input, @"^[!-_]{5}$");
+            if (!isValidInput)
+            {
+                Console.WriteLine("Input is incorrect! Please enter a string of 5 characters, where each character must be \"_\", \"-\", or \"!\"");
+            }
+            return isValidInput;
+        }
+
+        private bool IsWin()
+        {
+            // Check if the game has been won
+            return !answer.Contains('_');
+        }
+
+        public string GetHighestScoreGuess()
+        {
+            var bestGuess = heterogramScores
+                .Where(word => word.Key.All(letter => unguessedLetters.Contains(letter)
+                    && !misplacedLetters.ContainsKey(letter)
+                    && !misplacedLetters[letter].Contains(word.Key.IndexOf(letter))))
+                .OrderByDescending(word => word.Value)
+                .FirstOrDefault().Key;
+
+            return bestGuess ?? "arose";
+        }
+    }
+}
